92ebfc795a8162bf940b95fa79e8c872264ad7f1
Efrain May <efrain_may_75@hotmail.com>
2021-02-27 20:54:24 -0600

    obtener refernecia del objeto html del input

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+export class BusquedaComponent {
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
