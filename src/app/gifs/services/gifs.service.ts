ea2f95f92109558017ecc177c7be4e3cf0133751
Efrain May <efrain_may_75@hotmail.com>
2021-03-02 23:27:27 -0600

    optimizacion de la peticion http usando los params

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
@@ -7,8 +7,9 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  private apiKey: string = 'D7VaU8pAQcw2qsjrtnsaAZTpZobLhJVI';
-  private _historial: string[] = [];
+  private apiKey     : string = 'D7VaU8pAQcw2qsjrtnsaAZTpZobLhJVI';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
   //cambiar el any por su tipo
   public resultados: Gif[] = []; 
 
@@ -40,8 +41,15 @@ export class GifsService {
 
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
+
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
     
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=D7VaU8pAQcw2qsjrtnsaAZTpZobLhJVI&q=${query}&limit=10`)
+    console.log(params.toString());
+    
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
